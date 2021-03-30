81a265091630d93baacf31adf1b7c1b7a8d1fd61
Lyasinkovska <l.yasinkovska@gmail.com>
2021-03-30 19:03:10 +0300

    added formatting data for logging

--- airquality.py
+++ airquality.py
@@ -1,3 +1,4 @@
+import json
 import logging
 from twisted.internet.task import react
 from twisted.web.client import BrowserLikePolicyForHTTPS, Agent, readBody
@@ -9,7 +10,7 @@ METHOD = b'GET'
 HEADERS = {'accept': ['application/json']}
 urls = (BASE_URL + location_id for location_id in LOCATIONS_ID)
 
-logging.basicConfig(level=logging.INFO, filename='airquality.log', datefmt='%m-%d %H:%M', filemode='a',
+logging.basicConfig(level=logging.INFO, filename='airquality.log', datefmt='%m-%d-%Y %H:%M', filemode='a',
                     format='%(asctime)s %(message)s')
 
 
@@ -20,7 +21,20 @@ def callback_request(response):
 
 
 def callback_body(body):
-    logging.info('Response body: %s', body)
+    json_res = json.loads(body)
+    results = json_res.get('results')[0]
+
+    parameters = results.get('parameters')
+    params_names = [param.get('displayName') for param in parameters]
+    modeling_data = {'location_name': results.get('name'), 'country': results.get('country'),
+                     'coordinates': results.get('coordinates'), 'measured_parameters': params_names}
+
+    monitoring_data = [{'unit': params.get('unit'), 'param_name': params.get('displayName'),
+                        'last_value': params.get('lastValue'),
+                        'last_updated': params.get('lastUpdated'), 'parameter_id': params.get('parameterId')} for params
+                       in parameters]
+
+    logging.info('Modeling data: %s\nMonitoring data: %s', modeling_data, monitoring_data)
 
 
 def error_handler(error):
